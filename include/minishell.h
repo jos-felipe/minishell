d653c373841146db49a5f1cdea0f8262504f2c6f
Jose Felipe Dalcin Stieven <josfelip@c2r3p1.42sp.org.br>
2024-03-05 12:54:56 -0300

    filenames in unix style.

---
+++ include/minishell.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: josfelip <josfelip@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/30 10:52:24 by josfelip          #+#    #+#             */
+/*   Updated: 2024/03/05 12:26:38 by josfelip         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "../lib/includes/libft.h"
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#endif
