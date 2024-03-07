8a4969849959fbfa11ea6410bf7c599051ab63e0
Jose Felipe Dalcin Stieven <josfelip@c2r3p1.42sp.org.br>
2024-03-07 12:40:13 -0300

    evaluator.py tracked.

---
+++ qa/evaluator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import subprocess
+from modules import _00_parrot
+
+# Exit Status
+exit_status = 0
+
+# Make clean and Make
+clean = ["make", "clean", "-C", "../"]
+subprocess.run(clean, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+make = ["make", "-C", "../"]
+subprocess.run(make, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+# Colours
+GREEN = "\033[32;1m"
+RED = "\033[31;1m"
+COLOR_LIMITER = "\033[0m"
+colours = [GREEN, RED, COLOR_LIMITER]
+
+# Parrot prompt
+print("Parrot prompt")
+exit_status = _00_parrot.parrot(colours, exit_status)
+print("\n")
+
+subprocess.run(clean, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+if exit_status == 0:
+	print("🎉🎊🌟✨👏🎈🎈👏👏👏🎈🎈👏🌟✨🎉🎊\n")
+	
+exit(exit_status)
