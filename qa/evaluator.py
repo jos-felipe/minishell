4b8624824729c8c56e65ea1358ee5c917d739e9e
Guilherme Fantoni Borges <gfantoni@student.42sp.org.br>
2024-03-11 10:49:38 -0300

    launch executable test created.

--- qa/evaluator.py
+++ qa/evaluator.py
@@ -3,6 +3,7 @@
 import subprocess
 from modules import _00_parrot
 from modules import _01_signals
+from modules import _02_launch_executable
 
 # Exit Status
 exit_status = 0
@@ -27,6 +28,11 @@ print("\n")
 # Signals
 print("Signals")
 exit_status = _01_signals.signals(colours, exit_status)
+print("\n")
+
+# Search and launch executable
+print("Launch executable")
+exit_status = _02_launch_executable.launch_executable(colours, exit_status)
 print("\n")
 
 subprocess.run(clean, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
