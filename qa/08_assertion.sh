c5dbce7a64ba7091c9de332342445208b44f46c8
Jose Felipe Dalcin Stieven <josfelip@c2r3p1.42sp.org.br>
2024-03-14 17:54:56 -0300

    handle 0 has been implemented and validated.

--- qa/08_assertion.sh
+++ qa/08_assertion.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 STATUS=0
-../minishell < 08_command_list > outfile
+../minishell < 08_input > outfile
 cat outfile | grep 0 > /dev/null
 GREP_STATUS=$(echo $?)
 if [ "$GREP_STATUS" != "0" ]; then
