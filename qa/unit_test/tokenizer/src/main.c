e9f5fc991fcb480ebfa97b2d23e196c9c55aa016
Jose Felipe Dalcin Stieven <josfelip@c2r3p1.42sp.org.br>
2024-03-26 11:48:32 -0300

    linked file system for unit test

--- qa/unit_test/tokenizer/src/main.c
+++ qa/unit_test/tokenizer/src/main.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
+/*   By: josfelip <josfelip@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 17:09:14 by gfantoni          #+#    #+#             */
-/*   Updated: 2024/03/25 17:20:44 by gfantoni         ###   ########.fr       */
+/*   Updated: 2024/03/26 11:41:45 by josfelip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../../include/minishell.h"
+#include "../include/minishell.h"
 
 int main(int argc, char *argv[], char *envp[])
 {
