63bef527809ca69d1d11b327edb56e3abafbe717
Guilherme Fantoni Borges <gfantoni@student.42sp.org.br>
2024-04-15 15:57:32 -0300

    builtin_export test created.

---
+++ qa/unit_test/builtin_export/export_list.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import subprocess
+
+# Valgrind
+valgrind = "valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --suppressions=readline.supp -q --log-file=valgrind.log"
+
+# Colours
+GREEN = "\033[32;1m"
+RED = "\033[31;1m"
+COLOR_LIMITER = "\033[0m"
+colours = [GREEN, RED, COLOR_LIMITER]
+
+trash = subprocess.run("make -C expansion", stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+
+# Input Samples and Outputs references:
+
+# env_list
+input_data_list = ["\'export\'"]
+output_data_list = [f'1']
+
+i = 1
+for input_data, output_ref in zip(input_data_list, output_data_list):
+	output = subprocess.run(f"{valgrind} ./builtin_export/builtin_export {input_data}", stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+	outfile_content = output.stdout
+	if outfile_content == output_ref:
+		print(f"{colours[0]}{i}/{len(input_data_list)}.	OK  {colours[2]}")
+	else:
+		print(f"{colours[1]}{i}/{len(input_data_list)}.	KO  {colours[2]}")
+		
+	valgrind_status = subprocess.run('./valgrind.sh', stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+
+	# Check for leaks
+	if (valgrind_status.stdout == '0\n'):
+		print(f"{colours[0]}	MOK  {colours[2]}")
+	else:
+		print(f"{colours[1]}	MKO  {colours[2]}")
+	i = i + 1
+
+trash = subprocess.run("make fclean -C expansion", stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
---
+++ qa/unit_test/builtin_export/unit_tester.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+print("env_list")
+import env_list
+print("export_list")
+import export_list
