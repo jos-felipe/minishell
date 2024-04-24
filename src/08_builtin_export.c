252bd927c05cf94cae4970d4d539450241c0d033
Jose Felipe Dalcin Stieven <josfelip@c2r3p1.42sp.org.br>
2024-04-24 15:27:23 -0300

    bugfix: token doesn't get update

--- src/08_builtin_export.c
+++ src/08_builtin_export.c
@@ -6,11 +6,12 @@
 /*   By: josfelip <josfelip@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 16:56:46 by josfelip          #+#    #+#             */
-/*   Updated: 2024/04/16 14:50:09 by josfelip         ###   ########.fr       */
+/*   Updated: 2024/04/24 15:23:51 by josfelip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+#include "../include/dictionary.h"
 
 int	mini_export(t_token *arg, t_dict **env_list)
 {
@@ -22,7 +23,7 @@ int	mini_export(t_token *arg, t_dict **env_list)
 	cur = arg;
 	while (cur)
 	{	
-		tmp = mini_env_identifier(arg->token, env_list, 0, 0);
+		tmp = mini_env_identifier(cur->token, env_list, 0, 0);
 		if (tmp)
 			exit_status = tmp;
 		cur = cur->next;
