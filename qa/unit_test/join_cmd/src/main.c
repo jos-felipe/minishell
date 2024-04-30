7d208f4cf01892ba179940d6954ed5d1e5c9c611
Guilherme Fantoni Borges <gfantoni@student.42sp.org.br>
2024-04-30 10:08:31 -0300

    join_cmd tester created.

---
+++ qa/unit_test/join_cmd/src/main.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/11 17:09:14 by gfantoni          #+#    #+#             */
+/*   Updated: 2024/04/23 12:51:16 by gfantoni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+
+void	unit_print_redirect_list(t_cmd **head)
+{
+	t_cmd *node;
+
+	node = *head;
+	while (node)
+	{
+		printf("%d|%d", node->input_fd, node->output_fd);
+		printf("\n");
+		node = node->next;
+	}
+}
+
+void	mini_init(t_mini *pipex)
+{
+
+	pipex->path = NULL;
+	pipex->lst_memory = NULL;
+	pipex->status = 0;
+	pipex->cmd_line = NULL;
+	pipex->pathname = NULL;
+	pipex->token_list = NULL;
+	pipex->env_list = NULL;
+	pipex->cmd_list = NULL;
+	pipex->syntax_error = 0;
+}
+
+int main(int argc, char *argv[], char *envp[])
+{
+	t_mini	mini;
+
+	mini_init(&mini);
+	mini_getenv(&mini);
+	mini.cmd_line = argv[1];
+	mini_tokenizer(&mini);
+	mini_parser(&mini);
+	mini_expansion(&mini);
+	mini_redirect(&mini);
+	unit_print_redirect_list(&mini.cmd_list);
+	ft_free_trashman(ft_get_mem_address());
+	return (0);
+}
