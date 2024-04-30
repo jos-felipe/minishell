af419bf9a5931c0736846e1b72443e59d8184aa5
Guilherme Fantoni Borges <gfantoni@student.42sp.org.br>
2024-04-30 09:28:36 -0300

    'no redirect' test added to redirect tester.

--- qa/unit_test/redirect/unit_tester.py
+++ qa/unit_test/redirect/unit_tester.py
@@ -57,6 +57,9 @@ output_data_list.append(f'0|-1\n')
 input_data_list.append("\'< inblock < in1 cat\'")
 output_data_list.append(f'-1|1\n')
 
+input_data_list.append("\'ls -la | cat\'")
+output_data_list.append(f'0|1\n0|1\n')
+
 i = 1
 
 for input_data, output_ref in zip(input_data_list, output_data_list):
