2f96b8bce3d8a4db8d22a12242eace9a7e946cdf
Jose Felipe Dalcin Stieven <josfelip@c2r3p1.42sp.org.br>
2024-05-02 18:27:01 -0300

    new feature: check exit status

--- qa/unit_test/builtin_export/unit_tester.py
+++ qa/unit_test/builtin_export/unit_tester.py
@@ -32,86 +32,99 @@ test_description_list = [" - null value"]
 input_data_list = ["\'export var\' \'echo $var\'"]
 output_data_list = [f' ']
 err_data_list = [f'']
+exit_status_list = [0]
 
 #2
 test_description_list.append(" - single word")
 input_data_list.append("\'export var=jojo\' \'echo $var\'")
 output_data_list.append(f'jojo ')
 err_data_list.append(f'')
+exit_status_list.append(0)
 
-#3
 test_description_list.append(" - multiple words")
 input_data_list.append("\'export var=jojo kaka\' \'echo $var\'")
 output_data_list.append(f'jojo ')
 err_data_list.append(f'')
+exit_status_list.append(0)
 
-#4
 test_description_list.append(" - null value variant")
 input_data_list.append("\'export var=\' \'echo $var\'")
 output_data_list.append(f' ')
 err_data_list.append(f'')
+exit_status_list.append(0)
 
-#5
 test_description_list.append(" - invalid identifier with value")
 input_data_list.append("\'export =jojo\'")
 output_data_list.append(f'')
 err_data_list.append(f'export: not a valid identifier\n')
+exit_status_list.append(1)
 
 test_description_list.append(" - invalid identifier without value")
 input_data_list.append("\'export =\'")
 output_data_list.append(f'')
 err_data_list.append(f'export: not a valid identifier\n')
+exit_status_list.append(1)
 
 test_description_list.append(" - multiple variables")
 input_data_list.append("\'export var1=kaka var2=jojo\' \'echo $var1 $var2\'")
 output_data_list.append(f'kaka jojo ')
 err_data_list.append(f'')
+exit_status_list.append(0)
 
 test_description_list.append(" - multiple variables plus invalid identifier")
 input_data_list.append("\'export var1 var2=jojo =\' \'echo $var1 $var2\'")
 output_data_list.append(f' jojo ')
 err_data_list.append(f'export: not a valid identifier\n')
+exit_status_list.append(1)
 
 test_description_list.append(" - update variable content")
 input_data_list.append("\'export LANG=pt\' \'echo $LANG\'")
 output_data_list.append(f'pt ')
 err_data_list.append(f'')
+exit_status_list.append(0)
 
 test_description_list.append(" - don't update if null address")
 input_data_list.append("\'export LANG\' \'echo $LANG\'")
 output_data_list.append(f'en_US.UTF-8 ')
 err_data_list.append(f'')
+exit_status_list.append(0)
 
 test_description_list.append(" - update if null value")
 input_data_list.append("\'export LANG=\' \'echo $LANG\'")
 output_data_list.append(f' ')
 err_data_list.append(f'')
+exit_status_list.append(0)
 
 test_description_list.append(" - assign value using expansion")
 input_data_list.append("\'export var=$LANG\' \'echo $var\'")
 output_data_list.append(f'en_US.UTF-8 ')
 err_data_list.append(f'')
+exit_status_list.append(0)
 
 test_description_list.append(" - assign value using invalid expansion")
 input_data_list.append("\'export var=$affsdf\' \'echo $var\'")
 output_data_list.append(f' ')
 err_data_list.append(f'')
+exit_status_list.append(0)
 
 test_description_list.append(" - variable name starting with number")
 input_data_list.append("\'export 1var=jojo\' \'echo $1var\'")
 output_data_list.append(f' ')
 err_data_list.append(f'export: not a valid identifier\n')
+exit_status_list.append(1)
 
 test_description_list.append(" - variable name ending with number")
 input_data_list.append("\'export var1=jojo\' \'echo $var1\'")
 output_data_list.append(f'jojo ')
 err_data_list.append(f'')
+exit_status_list.append(0)
 
 test_description_list.append(" - export with no options")
 input_data_list.append("\'export SHLVL=2\' \'export\'")
 export_status = subprocess.run('bash -c "export"', stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
 output_data_list.append(export_status.stdout)
 err_data_list.append(f'')
+exit_status_list.append(0)
 
 # waiting for fix on "" tokenizier
 # input_data_list.append("\'export var=\"jojo kaka\"\' \'echo $var\'")
@@ -126,21 +139,24 @@ for input_data, output_ref, err_ref in zip(input_data_list, output_data_list, er
 	outfile_content = output.stdout
 	errfile_content = output.stderr
 	print(f"{colours[3]}{i}/{len(input_data_list)}{test_description_list[i-1]}{colours[0]}")
-	if outfile_content == output_ref and errfile_content == err_ref:
+	if outfile_content == output_ref and errfile_content == err_ref and output.returncode == exit_status_list[i-1]:
 		print(f"{colours[2]}	OK{colours[0]}")
 	else:
 		print(f"{colours[1]}	KO{colours[0]}")
 		if outfile_content != output_ref:
-			print(f"{colours[3]}stdout Diff{colours[0]}")
+			print(f"{colours[4]}stdout Diff{colours[0]}")
 			print(string_difference(output_ref, outfile_content))
 		if errfile_content != err_ref:
-			print(f"{colours[3]}stderr Diff{colours[0]}")
+			print(f"{colours[4]}stderr Diff{colours[0]}")
 			print(string_difference(err_ref, errfile_content))
+		if output.returncode != exit_status_list[i-1]:
+			print(f"{colours[4]}exit status{colours[0]}")
+			print(f"Expected: {exit_status_list[i-1]}")
+			print(f"Got: {output.returncode}")
 	
 	# Check for leaks
 	subprocess.run(f"{valgrind} ./builtin_export/unit.tester {input_data}", stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
 	valgrind_status = subprocess.run(f"./valgrind.sh", stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
-	# valgrind_status = subprocess.run('./valgrind.sh', stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
 	if (valgrind_status.stdout == '0\n'):
 		print(f"{colours[2]}	MOK{colours[0]}")
 	else:
