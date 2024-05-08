f6841b51975f0a76c0927ed4165e12aac469ece7
Guilherme Fantoni Borges <gfantoni@student.42sp.org.br>
2024-05-08 18:08:31 -0300

    some error when using three pipes.

--- qa/unit_test/execute/src/main.c
+++ qa/unit_test/execute/src/main.c
@@ -6,38 +6,11 @@
 /*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 17:09:14 by gfantoni          #+#    #+#             */
-/*   Updated: 2024/05/02 16:10:14 by gfantoni         ###   ########.fr       */
+/*   Updated: 2024/05/08 17:25:40 by gfantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
-
-// void	unit_print_cmd_exec_path(char *cmd_exec_path)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	if (cmd_exec_path)
-// 	{
-// 		while (cmd_exec_path[i])
-// 		{
-// 			printf("%s ", cmd_exec_path[i]);
-// 			i++;
-// 		}
-// 	}
-// }
-
-void	unit_print_cmd_exec_path(t_cmd **head)
-{
-	t_cmd *node;
-
-	node = *head;
-	while (node)
-	{
-		printf("%s\n", node->cmd_path);
-		node = node->next;
-	}
-}
 
 void	mini_init(t_mini *pipex)
 {
@@ -66,7 +39,6 @@ int main(int argc, char *argv[], char *envp[])
 	mini_redirect(&mini);
 	mini_execute(&mini);
 	
-	unit_print_cmd_exec_path(&mini.cmd_exec_list);
 	ft_free_trashman(ft_get_mem_address());
 	return (0);
 }
--- qa/unit_test/execute/unit_tester.py
+++ qa/unit_test/execute/unit_tester.py
@@ -20,22 +20,26 @@ subprocess.run("touch infile", shell=True)
 subprocess.run("echo apple, banana, carrot, bread > infile", shell=True)
 
 # Input Samples:
-input_data_list = ["\'cat < infile\'"]
+input_data_list = ["\'cat < infile > outfile\'"]
 output_data_list = [f'apple, banana, carrot, bread\n']
 
-input_data_list.append("\'cat -e < infile\'")
+input_data_list.append("\'cat -e < infile > outfile\'")
 output_data_list.append(f'apple, banana, carrot, bread$\n')
 
-input_data_list.append("\'cat < infile -e | tr , . | tr a-z A-Z\'")
+input_data_list.append("\'echo melvin | echo guinga > outfile\'")
+output_data_list.append(f'guinga\n')
+
+input_data_list.append("\'cat < infile -e | tr , . | tr a-z A-Z > outfile\'")
 output_data_list.append(f'APPLE. BANANA. CARROT. BREAD$\n')
 
-input_data_list.append("\'cat < infile -e | trX , . | echo melvin\'")
+input_data_list.append("\'cat < infile -e | trX , . | echo melvin > outfile\'")
 output_data_list.append(f'melvin\nCommand \'trX\' not found\n')
 
 i = 1
 
 for input_data, output_ref in zip(input_data_list, output_data_list):
-	output = subprocess.run(f"{valgrind} ./{name}/{name} {input_data}", stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+	trash = subprocess.run(f"{valgrind} ./{name}/{name} {input_data}", stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+	output = subprocess.run(f"cat outfile", stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
 	outfile_content = output.stdout
 	if outfile_content == output_ref:
 		print(f"{colours[0]}{i}/{len(input_data_list)}.	OK  {colours[2]}")
@@ -51,5 +55,5 @@ for input_data, output_ref in zip(input_data_list, output_data_list):
 		print(f"{colours[1]}	MKO  {colours[2]}")
 	i = i + 1
 
-trash = subprocess.run(f"rm infile", stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+trash = subprocess.run(f"rm infile outfile", stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
 trash = subprocess.run(f"make fclean -C {name}", stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
