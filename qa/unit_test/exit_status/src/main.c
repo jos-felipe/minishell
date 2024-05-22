01d2386dfda8df7741a556ff979b18b85b485b5d
Guilherme Fantoni Borges <gfantoni@student.42sp.org.br>
2024-05-22 13:03:29 -0300

    small correction in exit_status unit test.

--- qa/unit_test/exit_status/src/main.c
+++ qa/unit_test/exit_status/src/main.c
@@ -6,7 +6,7 @@
 /*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 17:09:14 by gfantoni          #+#    #+#             */
-/*   Updated: 2024/05/21 12:38:17 by gfantoni         ###   ########.fr       */
+/*   Updated: 2024/05/22 13:01:55 by gfantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,15 +43,14 @@ int main(int argc, char *argv[], char *envp[])
 	{
 		unit_print_exit_status(&mini);
 		ft_free_trashman(ft_get_mem_address());
+		ft_free_trashman_env(ft_get_mem_address_env());
 		return (0);
 	}
 	mini_parser(&mini);
 	mini_expansion(&mini);
 	mini_redirect(&mini);
 	mini_execute(&mini);
-
 	unit_print_exit_status(&mini);
-	
 	ft_free_trashman(ft_get_mem_address());
 	ft_free_trashman_env(ft_get_mem_address_env());
 	return (0);
