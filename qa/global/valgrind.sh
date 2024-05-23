c259c3fbee9e8bc8ed690c78df64cacd633d9b0b
Guilherme Fantoni Borges <gfantoni@student.42sp.org.br>
2024-05-23 14:57:37 -0300

    path to readline supp modifyed: MKO in global corrected.

--- qa/global/valgrind.sh
+++ qa/global/valgrind.sh
@@ -2,7 +2,7 @@
 
 STATUS=0
 VALGRIND_LOG=valgrind.log
-valgrind -q --leak-check=full --show-leak-kinds=all --track-origins=yes --suppressions=../readline.supp --log-file=$VALGRIND_LOG ../../minishell < file > /dev/null
+valgrind -q --leak-check=full --show-leak-kinds=all --track-origins=yes --suppressions=../../readline.supp --log-file=$VALGRIND_LOG ../../minishell < file > /dev/null
 VALGRIND_STATUS=$(cat $VALGRIND_LOG)
 if [ "$VALGRIND_STATUS" ]; then
 	STATUS=1
