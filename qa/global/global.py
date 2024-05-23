cacbacd4990e9084a2ed7d1e59f13a56bfa5f8bb
Guilherme Fantoni Borges <gfantoni@student.42sp.org.br>
2024-05-23 13:56:29 -0300

    A much better global tester is now running: Carols implementation.

--- qa/global/global.py
+++ qa/global/global.py
@@ -1,235 +1,63 @@
 #!/usr/bin/env python3
 
-# Unit name - the same as the directory name
-# unit = 'echo'
-
-# https://www.gnu.org/savannah-checkouts/gnu/bash/manual/html_node/Bash-Builtins.html#index-echo
-
 import subprocess
-
-# Function to process the file content and modify it
-def process_and_modify_file(filename):
-    lines = []
-    with open(filename, 'r+') as file:
-        for line in file:
-            if 'prompt >' in line:
-                value = line.split("prompt >")
-                if value[0] != '':
-                    lines.append(value[0])
-            else:
-                lines.append(line)
-        # lines.pop()
-    return lines
-
-# Valgrind
-valgrind = "valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --suppressions=readline.supp -q --log-file=valgrind.log"
+import os
 
 # Colours
 GREEN = "\033[32;1m"
 RED = "\033[31;1m"
 COLOR_LIMITER = "\033[0m"
-CYAN = "\033[36;1;3;208m"
-YELLOW = "\033[33;1m"
-colours = [COLOR_LIMITER, RED, GREEN, CYAN, YELLOW]
+colours = [GREEN, RED, COLOR_LIMITER]
 
-trash = subprocess.run(f"make -C ../../", stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+# Remove log file
+subprocess.run("rm log_file", shell=True)
 
-# Create input file
-infile = open('infile', 'w')
-infile.write('banana.apple.grape')
-infile.close()
+user = os.environ['USER']
 
+# INPUT REFERENCE LIST
+input_list = ["echo 42"] #1
+input_list.append("ERR") #2
 
-# Test description, Input Samples and Outputs references:
+# OUTPUT REFERENCE LIST
+output_ref_list = ["42\n"] #1
+output_ref_list.append("minishell: ERR: command not found\n") #2
 
-test_description_list = [" - one str"]
-stdin = "echo 42"
-stdin_list = [stdin]
-returned_instance = subprocess.run(f"bash -c '{stdin}'", stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
-stdout_list = [returned_instance.stdout]
-stderr_list = [returned_instance.stderr]
-returncode_list = [returned_instance.returncode]
+# OUTPUT EXIT CODE LIST
+output_exit_code = ["0\n"] #1
+output_exit_code.append("127\n") #2
 
-test_description_list.append(" - two str")
-stdin = "echo École 42"
-stdin_list.append(stdin)
-returned_instance = subprocess.run(f"bash -c '{stdin}'", stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
-stdout_list.append(returned_instance.stdout)
-stderr_list.append(returned_instance.stderr)
-returncode_list.append(returned_instance.returncode)
-
-test_description_list.append(" - two str with -n")
-stdin = "echo -n École 42"
-stdin_list.append(stdin)
-returned_instance = subprocess.run(f"bash -c '{stdin}'", stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
-stdout_list.append(returned_instance.stdout)
-stderr_list.append(returned_instance.stderr)
-returncode_list.append(returned_instance.returncode)
-
-test_description_list.append(" - two pipes and infile")
-stdin = "cat < infile -e | tr . , | tr a-z A-Z"
-stdin_list.append(stdin)
-returned_instance = subprocess.run(f"bash -c '{stdin}'", stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
-stdout_list.append(returned_instance.stdout)
-stderr_list.append(returned_instance.stderr)
-returncode_list.append(returned_instance.returncode)
-
-test_description_list.append(" - wrong command")
-stdin = "cat < infile -e | trERR . , | tr a-z A-Z"
-stdin_list.append(stdin)
-returned_instance = subprocess.run(f"bash -c '{stdin}'", stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
-stdout_list.append(returned_instance.stdout)
-stderr_list.append(returned_instance.stderr)
-returncode_list.append(returned_instance.returncode)
-
-test_description_list.append(" - wrong command")
-stdin = "echo a | echo b | echo c"
-stdin_list.append(stdin)
-returned_instance = subprocess.run(f"bash -c '{stdin}'", stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
-stdout_list.append(returned_instance.stdout)
-stderr_list.append(returned_instance.stderr)
-returncode_list.append(returned_instance.returncode)
-
-test_description_list.append(" - wrong command")
-stdin = "echo a | echoERR b | echo c"
-stdin_list.append(stdin)
-returned_instance = subprocess.run(f"bash -c '{stdin}'", stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
-stdout_list.append(returned_instance.stdout)
-stderr_list.append(returned_instance.stderr)
-returncode_list.append(returned_instance.returncode)
-
-test_description_list.append(" - wrong command")
-stdin = "echo \'melvin ropical"
-stdin_list.append(stdin)
-returned_instance = subprocess.run(f"bash -c '{stdin}'", stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
-stdout_list.append(returned_instance.stdout)
-stderr_list.append(returned_instance.stderr)
-returncode_list.append(returned_instance.returncode)
-
-test_description_list.append(" - simple expansion")
-stdin = "echo $LANGUAGE"
-stdin_list.append(stdin)
-returned_instance = subprocess.run(f"bash -c '{stdin}'", stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
-stdout_list.append(returned_instance.stdout)
-stderr_list.append(returned_instance.stderr)
-returncode_list.append(returned_instance.returncode)
-
-test_description_list.append(" - expand dollar sign")
-stdin = "echo $?"
-stdin_list.append(stdin)
-returned_instance = subprocess.run(f"bash -c '{stdin}'", stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
-stdout_list.append(returned_instance.stdout)
-stderr_list.append(returned_instance.stderr)
-returncode_list.append(returned_instance.returncode)
-
-test_description_list.append(" - expand dollar sign plus variable")
-stdin = "echo $?LANGUAGE"
-stdin_list.append(stdin)
-returned_instance = subprocess.run(f"bash -c '{stdin}'", stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
-stdout_list.append(returned_instance.stdout)
-stderr_list.append(returned_instance.stderr)
-returncode_list.append(returned_instance.returncode)
-
-test_description_list.append(" - expand with invalid in the middle")
-stdin = "echo $abc^def"
-stdin_list.append(stdin)
-returned_instance = subprocess.run(f"bash -c '{stdin}'", stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
-stdout_list.append(returned_instance.stdout)
-stderr_list.append(returned_instance.stderr)
-returncode_list.append(returned_instance.returncode)
-
-test_description_list.append(" - expand with invalid in the front")
-stdin = "echo $^abc"
-stdin_list.append(stdin)
-returned_instance = subprocess.run(f"bash -c '{stdin}'", stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
-stdout_list.append(returned_instance.stdout)
-stderr_list.append(returned_instance.stderr)
-returncode_list.append(returned_instance.returncode)
-
-test_description_list.append(" - simple expand with invalid in the middle")
-stdin = "echo $LANGUAGE^abc"
-stdin_list.append(stdin)
-returned_instance = subprocess.run(f"bash -c '{stdin}'", stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
-stdout_list.append(returned_instance.stdout)
-stderr_list.append(returned_instance.stderr)
-returncode_list.append(returned_instance.returncode)
-
-# Check for stdout, stderr and exit status
 i = 1
-for input_data, output_ref, err_ref in zip(stdin_list, stdout_list, stderr_list):
+for command, reference, reference_exit_code in zip(input_list, output_ref_list, output_exit_code):
+    command_exit_code = "\necho $?"
+    with open("file", "w") as file:
+        file.write(command + command_exit_code)
+    with open("ref_file", "w") as ref_file:
+        ref_file.write(reference)
+    with open("status_ref_file", "w") as status_ref_file:
+        status_ref_file.write(reference_exit_code)
+    output = subprocess.run("./script.sh", stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
 
-	# Open, write and close mini_infile
-	mini_infile = open(f"mini_infile", "w")
-	mini_infile.write(f"{input_data}")
-	mini_infile.close()
-
-	# Run the unit
-	output = subprocess.run(f"../../minishell < mini_infile", stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
-
-	# Open, write and close outfile, outfile_ref, errfile and erfile_ref
-	# Open
-	outfile_tmp = open(f"outfile_tmp", "w")
-	outfile_ref = open(f"outfile_ref", "w")
-	errfile = open(f"errfile", "w")
-	errfile_ref = open(f"errfile_ref", "w")
-	# Write
-	outfile_tmp.write(f"{output.stdout}\n")
-	outfile_ref.write(f"{output_ref}\n")
-	errfile.write(f"{output.stderr}\n")
-	errfile_ref.write(f"{err_ref}\n")
-	# Close
-	mini_infile.close()
-	outfile_tmp.close()
-	outfile_ref.close()
-	errfile.close()
-	errfile_ref.close()
-
-	# Clean outfile  for it to keep only the minishell output
-	lines = process_and_modify_file('outfile_tmp')
-	outfile = open('outfile', 'w')
-	outfile.writelines(lines)
-	outfile.close()
-	
-	print(f"{colours[3]}{i}/{len(stdin_list)}{test_description_list[i-1]}{colours[0]}")
-
-	# Check for stdout
-	diff_returned = subprocess.run(f"diff outfile_ref outfile", stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
-	if diff_returned.returncode != 0:
-		print(f"{colours[1]}	KO - stdout{colours[0]}")
-		print(f"{colours[4]}stdout diff{colours[0]}")
-		print(f"{diff_returned.stdout}")
-	else:
-		print(f"{colours[2]}	OK - stdout{colours[0]}")
-
-	# Check for stderr
-	diff_returned = subprocess.run(f"diff errfile_ref errfile", stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
-	if diff_returned.returncode != 0:
-		print(f"{colours[1]}	KO - stderr{colours[0]}")
-		print(f"{colours[4]}stderr diff{colours[0]}")
-		print(f"{diff_returned.stdout}")
-	else:
-		print(f"{colours[2]}	OK - stderr{colours[0]}")
-
-	# # Check for exit status
-	# if output.returncode != returncode_list[i-1]:
-	# 	print(f"{colours[4]}	KO - exit status{colours[0]}")
-	# 	print(f"{colours[4]}exit status diff{colours[0]}")
-	# 	print(f"Expected: {returncode_list[i-1]}")
-	# 	print(f"Got: {output.returncode}")
-	# else:
-	# 	print(f"{colours[2]}	OK - exit status{colours[0]}")
-	
-	# Check for leaks
-	subprocess.run(f"{valgrind} ../../minishell < mini_infile", stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
-	valgrind_status = subprocess.run(f"./valgrind.sh", stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
-	if (valgrind_status.stdout == '0\n'):
-		print(f"{colours[2]}	MOK{colours[0]}")
-	else:
-		print(f"{colours[1]}	MKO{colours[0]}")
-
-	print("\n------------------------------------\n")
-	i = i + 1
-
-# Clean
-subprocess.run(f"rm outfile outfile_ref errfile errfile_ref mini_infile outfile_tmp infile", shell=True)
-trash = subprocess.run(f"make fclean -C ../../", stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+    if (output.stdout == '0\n'):
+        print(f"{colours[0]}{i}/{len(input_list)}.\n	OK  {colours[2]}")
+    else:
+        print(f"{colours[1]}{i}/{len(input_list)}.\n	KO  {colours[2]}")
+    valgrind_status = subprocess.run('./valgrind.sh', stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+    if (valgrind_status.stdout == '0\n'):
+        print(f"{colours[0]}	MOK  {colours[2]}")
+    else:
+        print(f"{colours[1]}	MKO  {colours[2]}")
+    status = subprocess.run("./status.sh", stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+    if (status.stdout == '0\n'):
+        print(f"{colours[0]}	EOK  {colours[2]}")
+    else:
+        print(f"{colours[1]}	EKO  {colours[2]}")
+    with open("result", "r") as result_file:
+        result_command = result_file.read()
+    with open("status_result", "r") as status_result_file:
+        status_result = status_result_file.readlines()[-1]
+    with open("log_file", "a") as log_file:
+        log_file.write(f"{i}.\ncommand:\n{command}\nexpected output:\n{reference}\noutput received:\n{result_command}\nexpected status: {reference_exit_code}\nstatus received: {status_result}\n\n")
+    i = i + 1
+file_paths = [" ", "filename", "fdasdfsa", "file", "infile1", "infile2", "outfile1", "tudo", "outfile3", "ref_file", "outfile", "result", "status_file", "status_outfile", "status_ref_file", "status_result", "valgrind.log"]
+rm_command = ["rm", "-f"] + file_paths
+subprocess.run(rm_command)
--- src/97_utils_1.c
+++ src/97_utils_1.c
@@ -3,33 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   97_utils_1.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: josfelip <josfelip@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 16:22:08 by josfelip          #+#    #+#             */
-/*   Updated: 2024/05/20 16:31:51 by josfelip         ###   ########.fr       */
+/*   Updated: 2024/05/23 13:46:23 by gfantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/minishell.h"
-#include "../include/dictionary.h"
+// #include "../include/minishell.h"
+// #include "../include/dictionary.h"
 
-#define ERROR 200
+// #define ERROR 200
 
-int	mini_valid_identifier(char *str, t_dict **env_list, int start, int state)
-{
-	size_t	i;
+// int	mini_valid_identifier(char *str, t_dict **env_list, int start, int state)
+// {
+// 	size_t	i;
 
-	i = -1;
-	while (++i < ft_strlen(str) + 1)
-	{
-		state = mini_env_get_next_state(state, mini_env_get_column(str[i]));
-		if (state == ERROR)
-		{
-			ft_putstr_fd("bash: line 0: unset: `", STDERR_FILENO);
-			ft_putstr_fd(str, STDERR_FILENO);
-			ft_putstr_fd("': not a valid identifier\n", STDERR_FILENO);
-			return (1);
-		}
-	}
-	return (0);
-}
+// 	i = -1;
+// 	while (++i < ft_strlen(str) + 1)
+// 	{
+// 		state = mini_env_get_next_state(state, mini_env_get_column(str[i]));
+// 		if (state == ERROR)
+// 		{
+// 			ft_putstr_fd("bash: line 0: unset: `", STDERR_FILENO);
+// 			ft_putstr_fd(str, STDERR_FILENO);
+// 			ft_putstr_fd("': not a valid identifier\n", STDERR_FILENO);
+// 			return (1);
+// 		}
+// 	}
+// 	return (0);
+// }
