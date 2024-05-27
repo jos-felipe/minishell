319a4e7bbcbbd0f83656cd2f021be91379f4cb7d
Jose Felipe Dalcin Stieven <josfelip@c2r3p1.42sp.org.br>
2024-05-27 12:14:31 -0300

    Draft for cd with no directory

--- src/95_builtin_cd.c
+++ src/95_builtin_cd.c
@@ -6,15 +6,31 @@
 /*   By: josfelip <josfelip@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 14:34:02 by josfelip          #+#    #+#             */
-/*   Updated: 2024/05/27 11:57:22 by josfelip         ###   ########.fr       */
+/*   Updated: 2024/05/27 12:12:47 by josfelip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 #include "../include/dictionary.h"
 
-int		mini_cd(t_token *arg, t_dict **dict)
+int	mini_cd(t_token *arg, t_dict **dict)
 {
+	if (!arg->token)
+		mini_go_home();
 	ft_printf("%s\n", ft_dict_get_value(*dict, "PWD"));
 	return (0);
+}
+
+void	mini_go_home(t_dict *dict)
+{
+	char	var[2];
+	char	*old_path;
+	
+	old_path = ft_dict_get_value(dict, "OLDPWD");
+	var[0] = "PWD";
+	var[1] = ft_dict_get_value(dict, "HOME");
+	ft_dict_update(dict, var);
+	var[0] = "OLDPWD";
+	var[1] = old_path;
+	ft_dict_update(dict, var);
 }
