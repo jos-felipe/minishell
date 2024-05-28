f856b5941d4b5c0f3fb8e753206821600121943e
Jose Felipe Dalcin Stieven <josfelip@c2r3p1.42sp.org.br>
2024-05-28 12:19:31 -0300

    Output has been sorted and formatted

--- src/94_builtin_env.c
+++ src/94_builtin_env.c
@@ -6,7 +6,7 @@
 /*   By: josfelip <josfelip@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 14:34:02 by josfelip          #+#    #+#             */
-/*   Updated: 2024/05/28 12:05:22 by josfelip         ###   ########.fr       */
+/*   Updated: 2024/05/28 12:13:43 by josfelip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,11 @@ static void	mini_printenv(t_dict *dict)
 {
 	t_dict	*cur;
 	
-	cur = dict;
+	cur = ft_dictsort(dict);
 	while (cur)
 	{
 		if (cur->value)
-			ft_printf("%s=\"%s\"\n", cur->key, cur->value);
+			ft_printf("%s=%s\n", cur->key, cur->value);
 		else
 			ft_printf("%s\n", cur->key);
 		cur = cur->next;
