711d48ec4d410a141b7544d9c765d4305941494f
Jose Felipe Dalcin Stieven <josfelip@c2r3p1.42sp.org.br>
2024-06-03 14:13:03 -0300

    Using local free instead of delegate to trashman

--- src/96_builtin_pwd.c
+++ src/96_builtin_pwd.c
@@ -6,7 +6,7 @@
 /*   By: josfelip <josfelip@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 14:34:02 by josfelip          #+#    #+#             */
-/*   Updated: 2024/05/27 16:41:19 by josfelip         ###   ########.fr       */
+/*   Updated: 2024/06/03 14:12:14 by josfelip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int		mini_pwd(t_token *arg, t_dict **dict)
 	char	*cwd;
 	
 	cwd = getcwd(NULL, 0);
-	ft_collect_mem_env(cwd);
 	ft_printf("%s\n", cwd);
+	free(cwd);
 	return (0);
 }
