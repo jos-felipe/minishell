5ae74e02fb89453cb45bb608bb0b639c7f0590c4
Jose Felipe Dalcin Stieven <josfelip@c2r3p1.42sp.org.br>
2024-06-03 12:39:57 -0300

    Makefile has been adapted to deal with main.c outside src

---
+++ main.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: josfelip <josfelip@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/11 17:09:14 by gfantoni          #+#    #+#             */
+/*   Updated: 2024/06/03 12:15:47 by josfelip         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./include/minishell.h"
+
+int main(void)
+{
+	t_mini	mini;
+
+	mini_ctrl_signal();
+	mini_getenv(&mini);
+	mini.status = 0;
+	while (42)
+	{
+		mini_init(&mini);
+		mini.cmd_line = readline("prompt > ");
+		ft_collect_mem(mini.cmd_line);
+		if (mini.cmd_line == NULL)
+		{
+			mini_ctrl_d_exit();
+			break;
+		}
+		add_history(mini.cmd_line);
+		mini_tokenizer(&mini);
+		if (!mini.syntax_error)
+		{
+			mini_parser(&mini);
+			mini_expansion(&mini);
+			mini_redirect(&mini);
+			mini_execute(&mini);
+		}
+		ft_free_trashman(ft_get_mem_address());
+	}
+	ft_free_trashman(ft_get_mem_address());
+	ft_free_trashman_env(ft_get_mem_address_env());
+}
