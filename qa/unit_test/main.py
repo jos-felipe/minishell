d26deec507c9bc9ab14377a9e9c40adee493edd3
Jose Felipe Dalcin Stieven <josfelip@c2r3p1.42sp.org.br>
2024-06-04 15:05:53 -0300

    builtins echo cd pwd export unset env exit have been have been validated

--- qa/unit_test/main.py
+++ qa/unit_test/main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+status = 0
+
 print("\nUNIT TEST\n")
 
 # print("Tokenizer")
@@ -26,40 +28,39 @@ print("\nUNIT TEST\n")
 # print("execute")
 # from execute import unit_tester
 
-status = 0
 
-# from builtin_pwd.unit_tester import unit_pwd
-# _status = unit_pwd()
-# if _status != 0:
-# 	status = 1
+from echo.unit_tester import unit_echo
+_status = unit_echo()
+if _status != 0:
+	status = 1
 
-# from builtin_cd.unit_tester import unit_cd
-# _status = unit_cd()
-# if _status != 0:
-# 	status = 1
+from builtin_cd.unit_tester import unit_cd
+_status = unit_cd()
+if _status != 0:
+	status = 1
 
-# from echo.unit_tester import unit_echo
-# _status = unit_echo()
-# if _status != 0:
-# 	status = 1
+from builtin_pwd.unit_tester import unit_pwd
+_status = unit_pwd()
+if _status != 0:
+	status = 1
 
-# from builtin_env.unit_tester import unit_env
-# _status = unit_env()
-# if _status != 0:
-# 	status = 1
-
-# from builtin_export.unit_tester import unit_export
-# _status = unit_export()
-# if _status != 0:
-# 	status = 1
-
-# from builtin_exit.unit_tester import unit_exit
-# _status = unit_exit()
-# if _status != 0:
-# 	status = 1
+from builtin_export.unit_tester import unit_export
+_status = unit_export()
+if _status != 0:
+	status = 1
 
 from builtin_unset.unit_tester import unit_unset
 _status = unit_unset()
+if _status != 0:
+	status = 1
+
+from builtin_env.unit_tester import unit_env
+_status = unit_env()
+if _status != 0:
+	status = 1
+
+from builtin_exit.unit_tester import unit_exit
+_status = unit_exit()
 if _status != 0:
 	status = 1
 
