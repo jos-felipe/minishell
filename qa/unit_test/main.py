b5131ca02d4bf988be057d14a194b6c506270ef0
Guilherme Fantoni Borges <gfantoni@student.42sp.org.br>
2024-06-04 18:45:41 -0300

    all of the unit tests were placed in main.py

--- qa/unit_test/main.py
+++ qa/unit_test/main.py
@@ -4,30 +4,59 @@ status = 0
 
 print("\nUNIT TEST\n")
 
-# print("Tokenizer")
-# from tokenizer import unit_tester
+print("tokenizer")
+from tokenizer.unit_tester import unit_tokenizer
+_status = unit_tokenizer()
+if _status != 0:
+	status = 1
 
-# print("Syntax Error")
-# from syntax_error import unit_tester
+print("syntax error")
+from syntax_error.unit_tester import unit_syntax_error
+_status = unit_syntax_error()
+if _status != 0:
+	status = 1
 
-# print("Parser")
-# from parser import unit_tester
+print("parser")
+from parser.unit_tester import unit_parser
+_status = unit_parser()
+if _status != 0:
+	status = 1
 
-# print("Expansion")
-# from expansion import unit_tester
+print("expansion")
+from expansion.unit_tester import unit_expansion
+_status = unit_expansion()
+if _status != 0:
+	status = 1
 
-# print("Redirect")
-# from redirect import unit_tester
+print("redirect")
+from redirect.unit_tester import unit_redirect
+_status = unit_redirect()
+if _status != 0:
+	status = 1
 
-# print("join_cmd")
-# from join_cmd import unit_tester
+print("join cmd")
+from join_cmd.unit_tester import unit_join_cmd
+_status = unit_join_cmd()
+if _status != 0:
+	status = 1
 
-# print("get_cmd_path")
-# from get_cmd_path import unit_tester
+print("get cmd path")
+from get_cmd_path.unit_tester import unit_get_cmd_path
+_status = unit_get_cmd_path()
+if _status != 0:
+	status = 1
 
-# print("execute")
-# from execute import unit_tester
+print("execute")
+from execute.unit_tester import unit_execute
+_status = unit_execute()
+if _status != 0:
+	status = 1
 
+print("exit status")
+from exit_status.unit_tester import unit_exit_status
+_status = unit_exit_status()
+if _status != 0:
+	status = 1
 
 from echo.unit_tester import unit_echo
 _status = unit_echo()
