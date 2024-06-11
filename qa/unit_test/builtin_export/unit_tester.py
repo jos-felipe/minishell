d9c66632da48eb2d5e5de1dc92d03f3bf474cdc5
Jose Felipe Dalcin Stieven <josfelip@c2r3p1.42sp.org.br>
2024-06-11 17:21:13 -0300

    export with no options case has been restored to not deal with SHLVL=2

--- qa/unit_test/builtin_export/unit_tester.py
+++ qa/unit_test/builtin_export/unit_tester.py
@@ -124,7 +124,7 @@ def unit_export():
 
 	test_description_list.append(" - export with no options")
 	input_data_list.append("\'export SHLVL=2\' \'export\'")
-	export_status = subprocess.run('bash -c "export SHLVL=2; export"', stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+	export_status = subprocess.run('bash -c "export"', stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
 	output_data_list.append(export_status.stdout)
 	err_data_list.append(f'')
 	exit_status_list.append(0)
