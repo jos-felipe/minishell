2a17b5d081ecea17b933879c6b8ff94393a7e537
Guilherme Fantoni Borges <gfantoni@student.42sp.org.br>
2024-06-12 17:50:51 -0300

    commented lines removed.

--- src/02_ctrl_signal.c
+++ src/02_ctrl_signal.c
@@ -6,7 +6,7 @@
 /*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 16:42:04 by josfelip          #+#    #+#             */
-/*   Updated: 2024/06/11 16:01:49 by gfantoni         ###   ########.fr       */
+/*   Updated: 2024/06/12 13:35:32 by gfantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,16 @@
 void	mini_ctrl_signal(t_mini *mini)
 {
 	struct	sigaction int_action;
-	// struct	sigaction old_int_action;
 	struct	sigaction quit_action;
 
     int_action.sa_handler = sig_handler;
     sigemptyset(&int_action.sa_mask);
     int_action.sa_flags = 0;
 	sigaction(SIGINT, &int_action, NULL);
-    // old_int_action.sa_flags = 0;
     quit_action.sa_handler = SIG_IGN;
     quit_action.sa_flags = 0;
     sigemptyset(&quit_action.sa_mask);
 	sigaction(SIGQUIT, &quit_action, NULL);
-	// sigaction(SIGINT, NULL, &old_int_action);
-	// if (old_int_action.sa_handler != SIG_IGN)
 	mini->int_action = int_action;
 	mini->quit_action = quit_action;
 }
