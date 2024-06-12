92dc6cef44857b38c6b6ecded1671299c3bad2e7
Guilherme Fantoni Borges <gfantoni@student.42sp.org.br>
2024-06-12 18:01:58 -0300

    sig_handler_fork and sig_quit_handler functions removed from 02_utils.c

--- src/02_utils.c
+++ src/02_utils.c
@@ -6,7 +6,7 @@
 /*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 17:12:28 by gfantoni          #+#    #+#             */
-/*   Updated: 2024/06/12 11:02:20 by gfantoni         ###   ########.fr       */
+/*   Updated: 2024/06/12 17:05:01 by gfantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,23 @@
 
 void	sig_handler(int signum) 
 {
+	t_mini	*mini;
+	
 	if (signum == SIGINT)
 	{
 		rl_on_new_line();
 		rl_replace_line("", 0);
 		ft_putstr_fd("\n", 1);
 		rl_redisplay();
+		mini = get_mini_address();
+		mini->status = 130;
 	}
 }
 
-void	sig_handler_fork(int signum)
-{
-	if (signum == SIGINT)
-	{
-		ft_printf_fd(STDERR_FILENO, "\n");
-	}
-}
-
-void	sig_quit_handler(int signum)
-{
-	if (signum == SIGQUIT)
-	{
-		signal(SIGQUIT, SIG_DFL);
-	}
-}
+// void	sig_handler_fork(int signum)
+// {
+// 	if (signum == SIGINT)
+// 	{
+// 		ft_printf_fd(STDERR_FILENO, "\n");
+// 	}
+// }
