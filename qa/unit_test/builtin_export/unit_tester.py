d835081c7eeaa39b45ae0119f6cdce7b6c0cc709
Jose Felipe Dalcin Stieven <josfelip@c2r3p1.42sp.org.br>
2024-06-17 09:39:28 -0300

    builtin_export unit test: no sort outputs

--- qa/unit_test/builtin_export/unit_tester.py
+++ qa/unit_test/builtin_export/unit_tester.py
@@ -123,8 +123,8 @@ def unit_export():
 	exit_status_list.append(0)
 
 	test_description_list.append(" - export with no options")
-	input_data_list.append("\'export SHLVL=2\' \'export\' \'sort\'")
-	export_status = subprocess.run('bash -c "export | LC_COLLATE=C sort"', stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+	input_data_list.append("\'export SHLVL=2\' \'export\'")
+	export_status = subprocess.run('bash -c "export"', stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
 	output_data_list.append(export_status.stdout)
 	err_data_list.append(f'')
 	exit_status_list.append(0)
