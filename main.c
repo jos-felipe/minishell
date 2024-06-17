ea1eb631bbd4a6e7a9c52d2988fe258ef41720b9
Jose Felipe Dalcin Stieven <josfelip@c2r3p1.42sp.org.br>
2024-06-17 14:19:18 -0300

    .

--- main.c
+++ main.c
@@ -6,13 +6,14 @@
 /*   By: josfelip <josfelip@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 17:09:14 by gfantoni          #+#    #+#             */
-/*   Updated: 2024/06/17 14:12:36 by josfelip         ###   ########.fr       */
+/*   Updated: 2024/06/17 14:18:09 by josfelip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./include/minishell.h"
 
 static void	mini_no_error_detect(t_mini *mini);
+static void	mini_recursive_init(t_mini *mini);
 
 int	main(void)
 {
@@ -24,13 +25,7 @@ int	main(void)
 	mini->status = 0;
 	while (42)
 	{
-		mini_init(mini);
-		mini->int_action.sa_handler = sig_handler;
-		sigaction(SIGINT, &mini->int_action, NULL);
-		mini->quit_action.sa_handler = SIG_IGN;
-		sigaction(SIGQUIT, &mini->quit_action, NULL);
-		mini->cmd_line = readline("prompt > ");
-		ft_collect_mem(mini->cmd_line);
+		mini_recursive_init(mini);
 		if (mini->cmd_line == NULL)
 		{
 			mini_ctrl_d_exit();
@@ -53,4 +48,15 @@ void	mini_no_error_detect(t_mini *mini)
 	mini_expansion(mini);
 	mini_redirect(mini);
 	mini_execute(mini);
+}
+
+void	mini_recursive_init(t_mini *mini)
+{
+	mini_init(mini);
+	mini->int_action.sa_handler = sig_handler;
+	sigaction(SIGINT, &mini->int_action, NULL);
+	mini->quit_action.sa_handler = SIG_IGN;
+	sigaction(SIGQUIT, &mini->quit_action, NULL);
+	mini->cmd_line = readline("prompt > ");
+	ft_collect_mem(mini->cmd_line);
 }
